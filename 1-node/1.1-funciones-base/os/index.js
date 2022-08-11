98a7677c549cd55e2f436da8d6b20290a4a288ab
Silvia <scortesg9527@gmail.com>
2022-08-10 19:10:53 -0500

    os updated

--- 1-node/1.1-funciones-base/os/index.js
+++ 1-node/1.1-funciones-base/os/index.js
@@ -1,3 +1,11 @@
 const os = require('os'); //import things in node
-
-console.log('system version', os.version())
+console.log('system version:', os.version());
+console.log('System version name: ', os.platform());
+console.log('System version number:' + os.release());
+console.log('architecture: ',os.arch()); 
+console.log('Number of logical processors:' + os.cpus().length);
+console.log('Info about the CPUs: ', os.cpus());
+console.log(' free memory libre in bytes', os.freemem()); 
+console.log('folder of actual user: ', os.homedir()); 
+console.log('my ip address or my hostname (local): ', os.hostname());
+console.log('data info', os.userInfo())
