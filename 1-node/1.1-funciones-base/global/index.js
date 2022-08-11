66327efd3d14f32e1792f43f15cea68b556d5a52
Silvia <scortesg9527@gmail.com>
2022-08-10 19:38:51 -0500

    pull notes ready

---
+++ 1-node/1.1-funciones-base/global/index.js
@@ -0,0 +1,5 @@
+/* Logging the window object to the console. */
+console.log(this); //{} module.exports
+
+console.log('where my file is: ', __dirname)
+console.log('where my file is + file name: ', __filename)
