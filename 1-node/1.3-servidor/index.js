a0a49a3be35c8f3e7a75c15d5dc6c3e0ac1a87f1
Silvia <scortesg9527@gmail.com>
2022-08-10 21:27:44 -0500

    1.3 server practice

---
+++ 1-node/1.3-servidor/index.js
@@ -0,0 +1,12 @@
+// const { stat } = require('fs');
+const http = require('http');
+const server = http.createServer((request, response) =>{
+    const status = 200; //ok
+    const mimeType = {'Content-type' : 'text/html'};
+    response.writeHead(status, mimeType);
+    response.write('<html><body>HI FROM HTML NODEJS</body></html>');
+    response.end();
+});
+
+server.listen(9000);
+console.log('server is working');
