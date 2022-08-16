2f3dd1e964399b122c2498062aaab5a2c7dfe942
Silvia <scortesg9527@gmail.com>
2022-08-15 21:45:50 -0500

    practice 6 done

---
+++ js-advance-SilviaCG/1-node/1.6-media-server/index.js
@@ -0,0 +1,11 @@
+const http = require('http');
+const server = http.createServer((request, response) =>{
+    const status = 200; //ok
+    const mimeType = {'Content-type' : 'text/html'};
+    response.writeHead(status, mimeType);
+    response.write('<html><body>HI FROM HTML NODE JS silvye</body></html>');
+    response.end();
+});
+
+server.listen(9000);
+console.log('server is working');
