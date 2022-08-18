27d4146c1019efadd65d6e10bf8c8ff7aace1235
Silvia <scortesg9527@gmail.com>
2022-08-17 21:17:38 -0500

    feat: async problem

---
+++ js-advance-SilviaCG/3-async/2.1-sync-async.js
@@ -0,0 +1,22 @@
+/**
+ * codigo sincrono: una linea se ejecuta inmediatamente despues de la otra
+ * no hay codigo dependiente o bloqueante
+ * */
+
+// console.log('============== BLOQUE 1 ==============');
+//  console.log('Primero'); 
+//  console.log('Segundo');
+//  console.log('Tercero');
+
+ /**
+  * BLOQUE 2: PROBLEMA TIPICO DE ASYNC
+  */
+ const validatePayment = ()=>{
+    setTimeout(() =>{
+        console.log('2. validate CC and show loader');
+    }, 10000)
+ }
+
+ console.log('1 press button buy');
+ validatePayment();
+ console.log('3 message "thanks for your purchase"');
